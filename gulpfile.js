1bbf2060ce8cdbd6c3522dad420e27d95a72f406
Przemek Ćwiertka <przemek.cwiertka@gmail.com>
2023-01-06 20:32:36 +0100

    scss watch

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const watch = require('gulp-watch');
+
+gulp.task('css', function(){
+    return gulp.src('scss/styles.scss')
+    .pipe( sass() )
+    .pipe( gulp.dest('css') );
+})
+
+gulp.task('watch', function(){
+    gulp.watch('scss/**/*.scss', gulp.series('css'));
+})
+
+gulp.task('default', gulp.parallel('css', 'watch'));
