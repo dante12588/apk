de2d8b8bf2ab090d032d32da4826123bbc1b235b
Przemek Ćwiertka <przemek.cwiertka@gmail.com>
2023-01-07 23:44:29 +0100

    add Post

--- libs/Posts.php
+++ libs/Posts.php
@@ -12,4 +12,13 @@ class Posts{
         $this->db->query('SELECT * from posts');
         return $this->db->getResult();
     }
+
+    public function  addPost($title, $content, $category, $image = ''){
+        $this->db->query("INSERT INTO posts (title, content, category_id, image, status) VALUES (:title, :content, :category, :image, 'published')");
+        $this->db->bind(':title', $title);
+        $this->db->bind(':content', $content);
+        $this->db->bind(':category', $category);
+        $this->db->bind(':image', $image);
+        $this->db->exec();
+    }
 }
