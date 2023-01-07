b108c797f752f98628e9934c3f41b18b68429c3e
Przemek Ćwiertka <przemek.cwiertka@gmail.com>
2023-01-07 23:17:50 +0100

    default post image

--- templates/blog.php
+++ templates/blog.php
@@ -6,7 +6,10 @@ include 'include/header.php';
 
 <div class="n-width posts mt-100">
     <?php foreach($posts as $post){ ?>
-        <div class="post" style="--a: url('../upload/01/img.jpg')">
+        <div class="post" style="--a: url('../<?php if ($post['image']) {
+            echo $post['image'];} else {
+            echo 'img/post.jpg';
+            } ?>')">
             <h3 class="post__title"><?php echo $post['title'] ?></h3>
             <p class="post__content"><?php echo limit_text($post['content'], 20) ?></p>
             <span class="post__date"><?php echo postDate($post['date']); ?></span>
